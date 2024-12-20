62605053a188719c42bb7fa47cd051a0a290b3e5
Rodrigo Castro <castro.rl@gmail.com>
2024-12-19 23:02:31 -0700

    Updated with most recent projects

---
+++ src/scripts/imageReview.js
@@ -0,0 +1,10 @@
+// export function toggleImgProjectClass(event, element) {
+//   event.preventDefault();
+//   const img = element.querySelector("img");
+//   if (img) {
+//     img.classList.toggle("img-project");
+//   }
+// }
+
+// // Ensure the function is available globally
+// window.toggleImgProjectClass = toggleImgProjectClass;
